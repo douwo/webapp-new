a07faf4e81eaaef8643a5b46d38853c5e76a3ad3
douwo <874952595@qq.com>
2019-07-04 09:24:09 +0800

    Initial commit

---
+++ js/gotop.js
@@ -0,0 +1,19 @@
+$(function() {
+	$('.gotop').click(function() {
+		$('html , body').animate({
+			scrollTop: 0
+		}, 'slow');
+	});
+	$('.homepage').click(function() {
+		window.location.href = "/"
+	});	$(".gotop").hide(); 
+});
+
+$(window).scroll(function() {
+	// 滚动条距离顶部的距离 大于 200px时
+	if ($(window).scrollTop() >= 50) {
+		$(".gotop").fadeIn(1000); // 开始淡入
+	} else {
+		$(".gotop").stop(true, true).fadeOut(1000); // 如果小于等于 200 淡出
+	}
+});
---
+++ js/index.js
@@ -0,0 +1,33 @@
+// $('#reload').click(function(){
+// 	window.location.reload();
+// })
+// $('#reload').trigger("click");
+// $("#frist").on("tap", function() {
+// 	$(".card").removeClass("fadeInDown").addClass("fadeOutDown");
+// 	$("#frist").removeClass("fadeOutDown").addClass("heartBeat");
+	// setTimeout(function() {
+	// 	$("#frist").css({
+	// 		"margin-left": "0",
+	// 		"color": "#fff",
+	// 		"height": "45vw",
+	// 		"width": "100vw",
+	// 		"box-shadow": "none",
+	// 	});
+	// 	$(".footer").fadeOut();
+	// 	$(".banner>img").css({
+	// 		"top": "-38vh"
+	// 	})
+	// 	setTimeout(function() {
+	// 		$("#frist").css({
+	// 			"height": "100vh",
+	// 			"width": "100vw",
+	// 			"border-radius": "0",
+	// 			"margin-left": "0"
+	// 		});
+	// 		setTimeout(function(){
+	// 			window.location.href = "1.html"
+	// 		},200);
+	// 	}, 1000)
+	// 	$("#frist>img").fadeOut();
+	// }, 700)
+// })
